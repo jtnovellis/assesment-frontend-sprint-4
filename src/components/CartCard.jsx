24fef1b1bcbd6ef78f5bc1c24cad48552ce63f39
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-09-20 18:29:59 -0500

    cart page done

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
 
-const CartCard = () => {
+const CartCard = ({ quantity, image, title, price }) => {
   return (
     <div className='cartCard'>
-      <div className='cartCard__img'></div>
-      <div className='cartCard__info'></div>
-      <div className='cartCard__icons'></div>
+      <div className='cartCard__img'>
+        <img src={image} alt={title} loading='lazy' />
+      </div>
+      <div className='cartCard__info'>
+        <p className='cartCard__info--title'>{title}</p>
+        <p className='cartCard__info--price'>Price: ${price}</p>
+      </div>
+      <div className='cartCard__icons'>
+        <button className='cartCard__icons--button'>
+          <FontAwesomeIcon icon={faMinusCircle} />
+        </button>
+        <span>{quantity}</span>
+        <button className='cartCard__icons--button'>
+          <FontAwesomeIcon icon={faPlusCircle} />
+        </button>
+      </div>
     </div>
   );
 };
